7051a1ca027ad80796047712dfaf5a1a306b57e8
Jose Cedeño <josejecr97@gmail.com>
2019-01-24 14:51:31 -0800

    detectando eventos en JS

--- dom.js
+++ dom.js
@@ -5,3 +5,17 @@ console.log(h1);
 h1.innerHTML = "A un nuevo texto";
 h1.style.color = "red";
 h1.style.background = "blue";
+
+var boton = document.getElementById("boton");
+
+/*
+
+    boton.onclick = function(){
+        h1.style.color = "olive";
+    }
+
+*/
+
+boton.addEventListener('mouseover',function(){
+    console.log('me diste un click');
+})
