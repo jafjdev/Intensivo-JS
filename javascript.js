b43d8c3ebd1d1926fde6c4bdf9b6ad7a1dbdd272
Jose Cedeño <josejecr97@gmail.com>
2019-01-24 13:56:52 -0800

    Javascript.js
    Donde se pueden ver las diferencias de var const y let

---
+++ javascript.js
@@ -0,0 +1,36 @@
+alert('funcionando');
+
+console.log('var,let y const');
+
+
+/*
+let solo se puede inicializar 1 vez
+const tambien solo se puede 
+
+let edad= 55;
+let edad= 60;
+
+NO SE PUEDE HACER 
+*/
+
+/*
+
+for (var i = 0; i<=5; i++){
+    console.log(i);
+}
+console.log(i); --> i llega a 6
+
+*/
+
+/* 
+
+for (let i = 0; i<=5; i++){
+    console.log(i);
+}
+console.log(i); --> llega a 5 y no existe
+las variables let viven dentro de un
+ambiente en este caso, solo vive dentro 
+del for
+
+
+*/
